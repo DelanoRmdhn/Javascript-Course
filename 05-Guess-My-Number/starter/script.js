7ce20b16b7ab94701828b7ee87e0bbb9c8ae09e6
Delano <del.191006@gmail.com>
2025-07-10 22:16:20 +0700

    Adding input & validating user input

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//Membuat fitur input & jika user tidak menginput
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (guess == 0 || !guess) {
+    document.querySelector('.message').textContent = '❌ No Input received!';
+  }
+});
