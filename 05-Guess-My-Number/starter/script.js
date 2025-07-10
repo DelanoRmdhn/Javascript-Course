f7bb55edb4506a9b172c19795026809aca5bc870
Delano <del.191006@gmail.com>
2025-07-10 23:06:50 +0700

    Making the game logic (DOM Manipulation on HTML elements based if-else statement)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,29 @@
 'use strict';
 
+//deklarasi variabel
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 //Membuat fitur input & jika user tidak menginput
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
-  if (guess == 0 || !guess) {
+  if (!guess) {
     document.querySelector('.message').textContent = '❌ No Input received!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess) {
+    if (score > 1) {
+      score--;
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🧨 Game Over !';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
