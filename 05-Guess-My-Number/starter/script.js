3f1728b4388271561b9a058bb5988a26f1eb7ac5
Delano <del.191006@gmail.com>
2025-07-10 15:24:00 +0700

    [03. DOM & Events] : Introduction to Callback function @eventHandler (anonymous callback & named callback)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,3 +8,18 @@ console.log((document.querySelector('.number').textContent = 12));
 
 //Memanipulasi Elemen input HTML
 document.querySelector('.guess').value = 24;
+
+//Event Click
+
+document.querySelector('.check').addEventListener('click', function () {
+  const inputNum = document.querySelector('.guess').value;
+  console.log(inputNum);
+});
+
+let i = 1;
+const output = function (e) {
+  console.log(`tombol ${e.key} ditekan sebanyak ${i} kali.`);
+  i++;
+};
+
+document.querySelector('.again').addEventListener('keydown', output);
