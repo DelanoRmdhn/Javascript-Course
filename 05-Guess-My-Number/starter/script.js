8c63edce545dbd539d83bd5cfa698a7aa234be71
Delano <del.191006@gmail.com>
2025-07-10 14:19:40 +0700

    [03. DOM & Events] : introduction to DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Memanipulasi Elemen HTML
+const winningCondition = (document.querySelector('.message').textContent =
+  '🎉 Correct Number!');
+
+console.log((document.querySelector('.number').textContent = 12));
+
+//Memanipulasi Elemen input HTML
+document.querySelector('.guess').value = 24;
