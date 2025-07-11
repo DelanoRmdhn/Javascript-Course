9823e07f304b84ad1d92b1af79c15b6405ac74a5
Delano <del.191006@gmail.com>
2025-07-11 17:57:11 +0700

    [05.PROJECT GUESS MY NUMBER (FINISHED)] : callback reset, implementing game logic (reset & highscore), refactoring the code (make it DRY).

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,26 +1,58 @@
 'use strict';
 
+const generateSecretNumber = function () {
+  return Math.trunc(Math.random() * 20) + 1;
+};
+
 //deklarasi variabel
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = generateSecretNumber();
 let score = 20;
+let highScore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
+//function display
+const displayMassage = function (massage) {
+  document.querySelector('.message').textContent = massage;
+};
 
-//Callback function untuk backgroundColor
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+//function kondisi menang
 function winningCondition() {
+  displayMassage('🎉 Correct Number!');
   document.querySelector('body').style.backgroundColor = '#60b347';
   document.querySelector('.number').style.width = '30rem';
+  checkHighScsore();
 }
-//Membuat fitur input & jika user tidak menginput
+
+//function kondisi kalah
+function losingCondition() {
+  displayMassage('⛔ You Lost the Game!');
+  displayScore(0);
+  displayNumber('❌');
+}
+
+//function cek highcsore
+function checkHighScsore() {
+  if (score > highScore) {
+    document.querySelector('.highscore').textContent = score;
+    highScore = score;
+  }
+}
+
+//Event Handler 'check'
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No Input received!';
   } else if (guess === secretNumber) {
     winningCondition();
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess) {
     if (score > 1) {
       score--;
@@ -28,8 +60,26 @@ document.querySelector('.check').addEventListener('click', function () {
         guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '🧨 Game Over !';
-      document.querySelector('.score').textContent = 0;
+      losingCondition();
     }
   }
 });
+
+//callback function reset
+function reset() {
+  // generate ulang angka rahasia
+  secretNumber = generateSecretNumber();
+  score = 20; // reset skor juga
+
+  displayMassage('Start guessing...');
+  displayScore(score);
+  displayNumber('?');
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+  document.querySelector('.guess').value = '';
+}
+
+//Even Handler again
+document.querySelector('.again').addEventListener('click', reset);
