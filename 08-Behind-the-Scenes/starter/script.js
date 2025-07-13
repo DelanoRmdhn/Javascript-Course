29ec765958d8dd551718b1585daee1212250e01f
Delano <del.191006@gmail.com>
2025-07-13 16:29:17 +0700

    [08 : BTS Javascript] : Execution Context & Lexical Scoping with closure

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const name = 'Delano';
+
+const first = function () {
+  let a = 2;
+  let b = second(7, 5);
+  return a + b;
+};
+
+function second(x, y) {
+  let c = 2;
+  return c;
+}
+
+const count = first();
+
+function outer() {
+  let name = 'Delano';
+  return function inner() {
+    let age = 19;
+    console.log(`my name is ${name} im ${age} years old.`);
+  };
+}
+
+const information = outer();
+information();
